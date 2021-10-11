c73b0b7ebcce2b2946b6bd154d2a39fb7bdc8504
Yu-HuanWu <yuhuanhwu@gmail.com>
2021-10-11 10:06:41 -0700

    project set up: backend- set up user routes, frontend- set up store

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose")
+const db = require("./config/keys").mongoURI;
+const passport = require('passport');
+const User = require('./models/User');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+}
+
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log("Connected to MongoDB successfully"))
+    .catch(err => console.log(err));
+
+// app.use(express.json());
+// app.use(express.urlencoded());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+const port = process.env.PORT || 5000;
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets)
+
+
+app.listen(port, () => { console.log(`Listening on port ${port}`) });
