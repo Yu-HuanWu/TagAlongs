5004b711be5b1898174525f9cf3de4ea39fdc606
Yu-HuanWu <yuhuanhwu@gmail.com>
2021-10-11 14:08:19 -0700

    switched signup and signup container

--- frontend/src/components/session/signup_form_container.js
+++ frontend/src/components/session/signup_form_container.js
@@ -1,94 +1,18 @@
-import React from 'react';
-import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { signup } from '../../actions/session_actions';
+import SignupForm from './signup_form';
 
-class SignupForm extends React.Component {
-    constructor(props) {
-        super(props)
+const mapStateToProps = (state) => {
+    return {
+        signedIn: state.session.isSignedIn,
+        errors: state.errors.session
+    };
+};
 
-        this.state = {
-            email: '',
-            handle: '',
-            password: '',
-            password2: '',
-            errors: {}
-        }
-    }
-
-    componentWillReceiveProps(nextProps) {
-        if (nextProps.signedIn === true) {
-            this.props.history.push('/login');
-        }
-
-        this.setState({errors: nextProps.errors})
-    }
-
-    update(field) {
-        return e => this.setState({
-            [field]: e.currentTarget.value
-        });
-    }
-
-    handleSubmit(e) {
-        e.preventDefault();
-        let user = {
-            email: this.state.email,
-            handle: this.state.handle,
-            password: this.state.password,
-            password2: this.state.password2
-        };
-
-        this.props.signup(user, this.props.history); 
-    }
-
-    renderErrors() {
-        return(
-        <ul>
-            {Object.keys(this.state.errors).map((error, i) => (
-            <li key={`error-${i}`}>
-                {this.state.errors[error]}
-            </li>
-            ))}
-        </ul>
-        );
-    }
-
-    render() {
-        return (
-        <div className="signup-form-container">
-            <form onSubmit={this.handleSubmit}>
-            <div className="signup-form">
-                <br/>
-                <input type="text"
-                    value={this.state.email}
-                    onChange={this.update('email')}
-                    placeholder="Email"
-                />
-                <br/>
-                <input type="text"
-                    value={this.state.handle}
-                    onChange={this.update('handle')}
-                    placeholder="Handle"
-                />
-                <br/>
-                <input type="password"
-                    value={this.state.password}
-                    onChange={this.update('password')}
-                    placeholder="Password"
-                />
-                <br/>
-                <input type="password"
-                    value={this.state.password2}
-                    onChange={this.update('password2')}
-                    placeholder="Confirm Password"
-                />
-                <br/>
-                <input type="submit" value="Submit" />
-                {this.renderErrors()}
-            </div>
-            </form>
-        </div>
-        );
+const mapDispatchToProps = (dispatch) => {
+    return {
+        signup: user => dispatch(signup(user))
     }
 }
 
-export default withRouter(SignupForm);
+export default connect(mapStateToProps, mapDispatchToProps)(SignupForm);
