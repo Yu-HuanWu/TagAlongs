21caa395a57d6820fee3d94ce8a7788ad22473ac
jnardi1223 <77065234+jnardi1223@users.noreply.github.com>
2021-10-12 10:21:59 -0400

    nav bar out of splash

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -3,20 +3,16 @@ import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
 import { Switch } from 'react-router-dom';
-<<<<<<< HEAD
 import NavBar from "./navbar/nav_bar_container"
-import '../app.css'
-=======
-import NavBar from "./navbar/nav_bar"
 import '../app.scss'
->>>>>>> main
+
 
 const App = () => (
     <div className="body">
+        <div className="header">
+            <NavBar /> 
+        </div>
         <div className="splash">
-            <div className="header">
-                <NavBar /> 
-            </div>
             <div className="main">
                 <Switch>
                     {/* <AuthRoute exact path="/" component={MainPage} /> */}
