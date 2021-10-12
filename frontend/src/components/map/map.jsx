5333173875836b3a6b808449c543b0b0113acf61
Tony Zhang <tozhang665@gmail.com>
2021-10-12 15:23:54 -0700

    fixing map api

--- frontend/src/components/map/map.jsx
+++ frontend/src/components/map/map.jsx
@@ -1,32 +1,64 @@
-import React from 'react'
-import GoogleMapReact from 'google-map-react'
-import "./map.css"
+// import React from 'react'
+// import GoogleMapReact from 'google-map-react'
+// import "./map.css"
 
-class MapComponent extends React.Component{
-  
-  render(){
-    const center = {lat:37.7749,lng:-122.4194}
-    return(
-      <div>
-        this is the map component
-        <div id="map-component">
+// class MapComponent extends React.Component{
 
-        <GoogleMapReact 
-          bootstrapURLKeys={{key: process.env.REACT_APP_GOOGLE_API_KEY}}
-          defaultCenter={center}
-          center={center}
-          defaultZoom={12}
-          margin={[50,50,50,50]}
-          options={''}
-          onChange={()=>{}}
-          onChildClick={()=>{}}
-          >
+//   render(){
+//     const center = {lat:37.7749,lng:-122.4194}
+//     return(
+//       <div>
+//         this is the map component
+//         <div id="map-component">
 
-        </GoogleMapReact>
-          </div>
-      </div>
-    )
+//         {/* <GoogleMapReact 
+//           bootstrapURLKeys={{key: process.env.REACT_APP_GOOGLE_API_KEY}}
+//           defaultCenter={center}
+//           center={center}
+//           defaultZoom={12}
+//           margin={[50,50,50,50]}
+//           options={''}
+//           onChange={()=>{}}
+//           onChildClick={()=>{}}
+//           >
+
+//         </GoogleMapReact> */}
+
+//           </div>
+//       </div>
+//     )
+//   }
+// }
+
+// export default MapComponent
+
+
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
+
+const mapStyles = {
+  width: '100%',
+  height: '100%'
+};
+
+export class MapContainer extends Component {
+  render() {
+    return (
+      <Map
+        google={this.props.google}
+        zoom={13}
+        style={mapStyles}
+        initialCenter={
+          {
+            lat: 37.7749,
+            lng: -122.4194
+          }
+        }
+      />
+    );
   }
 }
 
-export default MapComponent
+export default GoogleApiWrapper({
+  apiKey: process.env.REACT_APP_GOOGLE_API_KEY
+})(MapContainer);
