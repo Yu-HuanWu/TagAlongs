60e105c69f8be98239ed9e148146a7bbd4cf7332
Yu-HuanWu <yuhuanhwu@gmail.com>
2021-10-12 23:13:16 -0700

    nav bar and background image test

--- frontend/src/components/navbar/nav_bar.js
+++ frontend/src/components/navbar/nav_bar.js
@@ -2,24 +2,21 @@ import React from "react";
 import { Link } from "react-router-dom";
 import './nav.scss'
 
-// let pathname = window.location.hash
-// console.log(pathname)
-
 class Nav extends React.Component{
     constructor(props) {
         super(props)
         this.state = {
-            toggle: true 
+            toggle: 'home' 
         }
 
         this.toggleButton = this.toggleButton.bind(this)
     }
     
     toggleButton() {
-        if (this.state.toggle === true) {
-            this.setState({toggle: false})
+        if (this.state.toggle === 'home' || this.state.toggle === 'login') {
+            this.setState({toggle: 'signup'})
         } else {
-            this.setState({toggle: true})
+            this.setState({toggle: 'login'})
         }
     }; 
     
@@ -33,25 +30,21 @@ class Nav extends React.Component{
                     <Link to="/">
                         TagAlongs
                     </Link>
-                    <div>
+                    <div className="nav-user-info">
                         <p className="users-name">Hello, {user.handle}</p>
+                        <span className="nav-divider"/>
+                        <button className="nav-buttons">Profile</button>
+                        <span className="nav-divider"/>
+                        <button className="nav-buttons">What's a TagAlong</button>
+                        <span className="nav-divider"/>
+                        <button className="nav-buttons">About us</button>
+                        
                         <button className="logout-button" onClick={logout}>Sign Out</button>
                     </div>
                 </div>
             )
         } else {
-            // return (
-            //     <div className="main-nav">
-            //         <Link to="/">
-            //             TagAlongs
-            //         </Link>
-            //         <div className="user-auth">
-            //             <Link className="sign-up-button" to="/signup">Sign Up</Link>
-            //             <Link className="sign-in-button" to="/login">Log In</Link>
-            //         </div>
-            //     </div>
-            // )
-            if (this.state.toggle) {
+            if (this.state.toggle === 'signup') {
             return (
                 <div className="main-nav">
                     <Link to="/">
@@ -62,72 +55,33 @@ class Nav extends React.Component{
                         <Link onClick={this.toggleButton} className="nav-button" to="/login">Log In</Link>
                     </div>
                 </div>
-            )} else {
-            return (
-                <div className="main-nav">
-                <Link to="/">
-                    TagAlongs
-                </Link>
-                <div className="user-auth">
-                    <Link onClick={this.toggleButton} className="nav-button" to="/signup">Sign Up</Link>
-                    <Link className="nav-button-clicked" to="/login">Log In</Link>
+            )} else if (this.state.toggle === 'login') {
+                return (
+                    <div className="main-nav">
+                    <Link to="/">
+                        TagAlongs
+                    </Link>
+                    <div className="user-auth">
+                        <Link onClick={this.toggleButton} className="nav-button" to="/signup">Sign Up</Link>
+                        <Link className="nav-button-clicked" to="/login">Log In</Link>
+                    </div>
                 </div>
-            </div>
-            )}
+            )} else {
+                return (
+                    <div className="main-nav">
+                        <Link to="/">
+                            TagAlongs
+                        </Link>
+                        <div className="user-auth">
+                            <Link onClick={this.toggleButton} className="nav-button" to="/signup">Sign Up</Link>
+                            <Link onClick={this.toggleButton} className="nav-button" to="/login">Log In</Link>
+                        </div>
+                    </div>
+                )
+            }
         }   
     }
 }
-// const Nav = ({ user, logout }) => {
-    // console.log(toggle)
-    // if (user && user.handle) {
-    //     return (
-    //         <div className="main-nav">
-    //             <Link to="/">
-    //                 TagAlongs
-    //             </Link>
-    //             <div>
-    //                 <p className="users-name">Hello, {user.handle}</p>
-    //             <button className="logout-button" onClick={logout}>Sign Out</button>
-    //             </div>
-    //         </div>
-    //     )
-    // } else {
-    //     // return (
-    //     //     <div className="main-nav">
-    //     //         <Link to="/">
-    //     //             TagAlongs
-    //     //         </Link>
-    //     //         <div className="user-auth">
-    //     //             <Link className="sign-up-button" to="/signup">Sign Up</Link>
-    //     //             <Link className="sign-in-button" to="/login">Log In</Link>
-    //     //         </div>
-    //     //     </div>
-    //     // )
-    //     if (pathname === "#/login") {
-    //     return (
-    //         <div className="main-nav">
-    //             <Link to="/">
-    //                 TagAlongs
-    //             </Link>
-    //             <div className="user-auth">
-    //                 <Link onClick={toggleButton()} className="sign-up-button" to="/signup">Sign Up()</Link>
-    //                 <Link className="sign-in-button" to="/login">Log In</Link>
-    //             </div>
-    //         </div>
-    //     )} else {
-    //     return (
-    //         <div className="main-nav">
-    //         <Link to="/">
-    //             TagAlongs
-    //         </Link>
-    //         <div className="user-auth">
-    //             <Link onClick={toggleButton()} className="sign-up-button" to="/signup">Sign Up</Link>
-    //             <Link className="sign-in-button" to="/login">Log In()</Link>
-    //         </div>
-    //     </div>
-    //     )}
-    // }
-    
-// }
+
 
 export default Nav
