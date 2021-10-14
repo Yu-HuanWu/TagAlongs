a7763e9847c54c298f6a9a75c75abbdc4d43e014
atjohnfeng <atjohnfeng@gmail.com>
2021-10-13 18:05:05 -0700

    additional user profile html and js

--- frontend/src/components/users/user_profile.jsx
+++ frontend/src/components/users/user_profile.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import './user_profile.scss';
+import defaultAvatar from './avatars/default.png'
 
 class UserProfile extends React.Component {
 
@@ -12,18 +13,37 @@ class UserProfile extends React.Component {
         }
     }
 
+    renderAvatar() {
+        switch(this.state.avatar) {
+            case 'default':
+                return <img src={ defaultAvatar } 
+                            alt="default-avatar" 
+                            className="user-profile-avatar" />
+            default:
+                return <img src={ defaultAvatar }
+                            alt="default-avatar" 
+                            className="user-profile-avatar" />
+        }
+    }
+
     render() {
         const user = this.props.currentUser;
         return (
             <div className="user-profile">
                 <div className="user-profile-left">
-                    {console.log(user)}
+                    { this.renderAvatar() }
                     <h1>{ user.firstName.toUpperCase() } { user.lastName.toUpperCase() }</h1>
                     <p>Earned <b>{ user.rating }</b> Cookies</p>
                     <p>Tagged Along <b>{ user.tagAlongsCompleted }</b> Times</p>
+                    <div className="user-profile-nav">
+                        <ul className="user-profile-nav-list">
+
+                        </ul>
+                    </div>
                 </div>
                 <div className="user-profile-right">
                     <div className="user-tagalongs">
+                        {/* { this.renderRightProfile() } */}
                         Temp
                     </div>
                 </div>
