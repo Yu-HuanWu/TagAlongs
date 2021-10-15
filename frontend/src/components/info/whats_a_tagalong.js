3596c8b7eed06683bae1c7b6f5588d28757a2a0f
jnardi1223 <77065234+jnardi1223@users.noreply.github.com>
2021-10-15 00:03:53 -0400

    added to tagalongs page

--- frontend/src/components/info/whats_a_tagalong.js
+++ frontend/src/components/info/whats_a_tagalong.js
@@ -5,7 +5,7 @@ const Whatsatagalong = () => {
         <div className="main-textbox">
             <div className="textbox">
                 <h1 className="question-title">What is a TagAlong?</h1>
-                    <p className="info-para" >The creators of TagAlongs believe people should be able to go about their daily routines with peace of mind. For this reason, they created a way for people
+                    <p className="info-para" >The creators of TagAlongs believe people should be able to go about their daily routines with peace of mind. For this reason, they created a way 
                         to request companionship from trusted volunteers to acompany them on their route. A user simply signs into their account and adds some basic information about their route. Once 
                         is in the system, volunteers in your vacinity will be able to accept your request.
                     </p>
@@ -17,7 +17,8 @@ const Whatsatagalong = () => {
                 <h1 className="question-title">Become a Volunteer</h1>
                     <p className="quote">“The best way to find yourself is to lose yourself in the service of others.” – Gandhi
                     </p>
-                    <p className="info-para">Sign up today and serve those 
+                    <p className="info-para">Sign up is quick and easy. Once apporved, a volunteer will be able to accept TagAlongs in their area. Tagalongs come in all shapes and sizes so be sure to check 
+                    tags on the request so you can come prepared. Once a trip is comopleteled a volunteer can check their profile and see how many "upcookies" they have accumulated from their generousity! 
                     </p>
             </div>
         </div>
