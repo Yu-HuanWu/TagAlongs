23037c1cac6132eb22c6d30b895ba00089d72b31
jnardi1223 <77065234+jnardi1223@users.noreply.github.com>
2021-10-15 01:19:13 -0400

    added resources page

--- frontend/src/components/info/resources.js
+++ frontend/src/components/info/resources.js
@@ -1,11 +1,42 @@
 import React from "react"
 import './info.scss'
+import SAH from "./img/SAH.jpeg"
+import TrevorProject from "./img/trevor_project.jpeg"
+import SPL from "./img/SPL_logo.jpeg"
 
 const Resources = () => {
     return (
         <div className="main-textbox">
             <div className="textbox">
-                Resources
+                <h1 className="resource-title">Stop Asian American and Pacific Islander Hate</h1>
+                    <div className="resources-box">
+                        <img className="resources-img"src={SAH}/>
+                        <div className="info-para-r">In response to the alarming escalation in xenophobia and bigotry resulting from the COVID-19 pandemic, 
+                            the Asian Pacific Policy and Planning Council (A3PCON), Chinese for Affirmative Action (CAA),
+                            and the Asian American Studies Department of San Francisco State University launched the Stop AAPI Hate coalition on March 19, 2020. 
+                            The coalition tracks and responds to incidents of hate, violence, harassment, discrimination, shunning, and child bullying against Asian 
+                            Americans and Pacific Islanders in the United States. You can visit their website <a href="https://stopaapihate.org/"  target="_blank">here</a>
+                        </div>
+                    </div>
+            </div>
+            <div className="textbox">
+                <h1 className="resource-title">The Trevor Project</h1>
+                    <div className="resources-box">
+                        <div className="info-para-r">The Trevor Project is an American nonprofit organization founded in 1998 focused on suicide prevention efforts among 
+                        lesbian, gay, bisexual, transgender, queer, and questioning youth. Through a toll-free telephone number, it operates The Trevor Lifeline, a confidential 
+                        service that offers trained counselors. More information is available <a href="https://www.thetrevorproject.org/donate/?gclid=EAIaIQobChMIvYvWwpHL8wIV_w2tBh1Juw5BEAAYASAAEgIY6fD_BwE"  target="_blank">here</a>
+                        </div>
+                        <img className="resources-img"src={TrevorProject}/>
+                    </div>
+            </div>
+            <div className="textbox">
+                <h1 className="resource-title">The Trevor Project</h1>
+                    <div className="resources-box">
+                        <img className="resources-img"src={SPL}/>
+                        <div className="info-para-r">The SPLC is a catalyst for racial justice in the South and beyond, working in partnership with communities to dismantle white supremacy, strengthen 
+                        intersectional movements, and advance the human rights of all people. More information is available <a href="https://www.splcenter.org/"  target="_blank">here</a>
+                        </div>
+                    </div>
             </div>
         </div>
 
--- frontend/src/components/info/whats_a_tagalong.js
+++ frontend/src/components/info/whats_a_tagalong.js
@@ -7,11 +7,11 @@ const Whatsatagalong = () => {
                 <h1 className="question-title">What is a TagAlong?</h1>
                     <p className="info-para" >The creators of TagAlongs believe people should be able to go about their daily routines with peace of mind. For this reason, they created a way 
                         to request companionship from trusted volunteers to acompany them on their route. A user simply signs into their account and adds some basic information about their route. Once 
-                        is in the system, volunteers in your vacinity will be able to accept your request.
+                        is in the system, volunteers in your vacinity will be able to accept the request.
                     </p>
                 <h1 className="question-title">Request a TagAlong</h1>
                     <p className="info-para" >If you or someone you know would like a trusted volunteer to join you on your next outing please sign up today! Whether it's an everyday errand, grocery trip, 
-                        or just a walk in the park, our volunteers are ready escort you with a smile. Once your trip is completed please don't forget to "upcookie" or "downcookie" 
+                        or just a walk in the park, our volunteers are ready escort you with a smile. Once your trip is completed please, don't forget to "upcookie" or "downcookie" 
                         your companion. Your feedback is vital to assuring all TagAlong volunteers provide a comfortable eviroment for all. 
                     </p>
                 <h1 className="question-title">Become a Volunteer</h1>
