8609f104165fc8cc1c453d27af821049d3b44e38
atjohnfeng <atjohnfeng@gmail.com>
2021-10-14 20:32:22 -0700

    fixed navbar issue on tagalong show

--- routes/api/users.js
+++ routes/api/users.js
@@ -113,7 +113,7 @@ router.post('/login', (req, res) => {
                         jwt.sign(
                             payload,
                             keys.secretOrKey,
-                            { expiresIn: 3600 },
+                            { expiresIn: 36000 },
                             (err, token) => {
                                 res.json({
                                     success: true,
