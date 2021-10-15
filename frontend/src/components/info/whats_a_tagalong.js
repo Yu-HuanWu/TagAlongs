cd8375de1396a790ae23794e46359986634f035a
jnardi1223 <77065234+jnardi1223@users.noreply.github.com>
2021-10-14 23:53:09 -0400

    working on whatsatagalong page

--- frontend/src/components/info/whats_a_tagalong.js
+++ frontend/src/components/info/whats_a_tagalong.js
@@ -4,9 +4,21 @@ const Whatsatagalong = () => {
     return (
         <div className="main-textbox">
             <div className="textbox">
-                <h1>What is a TagAlong?</h1>
-                <p>The creaters of TagAlongs believe people should be able to go about their daily routines with peace of mind. For this reason, they created a way for people
-                 to request companionship from trusted volunteers to acompany them on their route. </p>
+                <h1 className="question-title">What is a TagAlong?</h1>
+                    <p className="info-para" >The creators of TagAlongs believe people should be able to go about their daily routines with peace of mind. For this reason, they created a way for people
+                        to request companionship from trusted volunteers to acompany them on their route. A user simply signs into their account and adds some basic information about their route. Once 
+                        is in the system, volunteers in your vacinity will be able to accept your request.
+                    </p>
+                <h1 className="question-title">Request a TagAlong</h1>
+                    <p className="info-para" >If you or someone you know would like a trusted volunteer to join you on your next outing please sign up today! Whether it's an everyday errand, grocery trip, 
+                        or just a walk in the park, our volunteers are ready escort you with a smile. Once your trip is completed please don't forget to "upcookie" or "downcookie" 
+                        your companion. Your feedback is vital to assuring all TagAlong volunteers provide a comfortable eviroment for all. 
+                    </p>
+                <h1 className="question-title">Become a Volunteer</h1>
+                    <p className="quote">“The best way to find yourself is to lose yourself in the service of others.” – Gandhi
+                    </p>
+                    <p className="info-para">Sign up today and serve those 
+                    </p>
             </div>
         </div>
     )
