c0657dedbbbed7aa835ee8be4a4cef1f319ef5d3
atjohnfeng <atjohnfeng@gmail.com>
2021-10-17 20:58:22 -0700

    Add rating container

--- frontend/src/components/ratings/rating.jsx
+++ frontend/src/components/ratings/rating.jsx
@@ -2,3 +2,28 @@ import UpCookie from './images/upcookie.svg';
 import DownCookie from './images/downcookie.svg';
 import React from 'react';
 import './review.scss';
+
+class RatingForm extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    sendRating() {
+
+    }
+
+    render() {
+        return (
+            <div className="cookie-form-container">
+                <ul className="cookie-form">
+                    <li><img src={downCookie} 
+                            onClick={() => sendRating()} />
+                    </li>
+                    <li><img src={upCookie} 
+                            onClick={() => sendRating()} />
+                    </li>
+                </ul>
+            </div>
+        )
+    }
+}
