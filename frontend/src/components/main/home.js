85bd9a7e21b36eae25155f512b7765e54062d0df
Yu-HuanWu <yuhuanhwu@gmail.com>
2021-10-24 21:54:39 -0700

    fixed some typo from spalsh page and wpulled updates from Jim

--- frontend/src/components/main/home.js
+++ frontend/src/components/main/home.js
@@ -25,18 +25,18 @@ class Home extends React.Component {
                             <h1 className="info-title">Who?</h1>
                             <br />
                             <p>Our volunteer program is a welcoming open community that shares a passion for helping others. 
-                                Volunteers and users come from all walks of life and deeply care about their neighbors and their safty.</p>
+                                Volunteers and users come from all walks of life and deeply care about their neighbors and their safety.</p>
                         </div>
                         <div className="info-box">
                             <h1 className="info-title">What?</h1>
                             <br />
-                            <p>TagAlongs is a Volunteer based Buddy system application that allows users to request companionship for their daily routes or activities. 
-                                Volunteers contribute by accepting TagAlongs in their area, meeting with the user, and providing a safe enviroment for the user to travel in.</p>
+                            <p>TagAlongs is a volunteer-based Buddy system application that allows users to request companionship for their daily routes or activities. 
+                                Volunteers contribute by accepting TagAlongs in their area, meeting with the user, and providing a safe environment for the user to travel in.</p>
                         </div>
                         <div className="info-box">
                             <h1 className="info-title">How?</h1>
                             <br/>
-                            <p>Please sign up and<Link to="/whatsatagalong"> read more </Link> about how to use our service for youself or a loved one. Or sign up to be a volunteers!</p>
+                            <p>Please sign up and<Link to="/whatsatagalong"> read more </Link> about how to use our service for yourself or a loved one. Or sign up to be a volunteer!</p>
                         </div>
                     </div>
                 </div>
